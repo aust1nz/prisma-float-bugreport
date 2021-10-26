a9f5986b3e055eddb1ec676d0946dd1bd869bbcb
Austin Zentz <austin.zentz@kippdc.org>
2021-10-26 09:47:25 -0400

    Include a workaround

--- src/insert.ts
+++ src/insert.ts
@@ -2,25 +2,25 @@ import { PrismaClient } from "@prisma/client";
 
 const employees = [
   {
-    title: "Test Person Number 1",
+    title: "Test Person 'Number 1'",
     salary: 45000,
     fte: 1,
     fteAlternate: 1,
   },
   {
-    title: "Test Person Number 2",
+    title: "Test Person 'Number 2'",
     salary: 45000,
     fte: 1,
     fteAlternate: 1,
   },
   {
-    title: "Test Person Number 3",
+    title: "Test Person 'Number 3'",
     salary: 45000,
     fte: 1,
     fteAlternate: 1,
   },
   {
-    title: "Test Person Number 4",
+    title: "Test Person 'Number 4'",
     salary: 45000,
     fte: 1,
     fteAlternate: 1,
@@ -47,11 +47,27 @@ const insert = async () => {
 
   // This throws an error
   // Raw query failed. Code: `22P03`. Message: `db error: ERROR: incorrect binary data format in bind parameter 3`
-  await prisma.$executeRaw`
+  try {
+    await prisma.$executeRaw`
     INSERT INTO "Employee"
                 (title, salary, "fteAlternate")
     VALUES (${employees[3].title}, ${employees[3].salary}, ${employees[3].fteAlternate});
   `;
+  } catch (err) {
+    console.log(err);
+  }
+
+  // This is a current workaround: manually sanitize floats and insert them directly
+  // into an executeRawUnsafe call; parameterize the rest.
+  await prisma.$executeRawUnsafe(
+    `
+    INSERT INTO "Employee"
+                (title, salary, fte, "fteAlternate")
+    VALUES ($1, $2, ${employees[3].fte}, ${employees[3].fteAlternate});
+  `,
+    employees[3].title,
+    employees[3].salary
+  );
 };
 
 insert();
